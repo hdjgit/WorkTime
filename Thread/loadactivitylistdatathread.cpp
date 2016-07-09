713b1667bb6946b1342027e0ac7890924052a073
hdj <815752338@qq.com>
2016-07-09 11:24:32 +0800

    从数据库加载数据ok，但是数据库无缘无故崩溃了，而且找不到错误提示 决定采用其他持久化的方式取代数据库方式

---
+++ Thread/loadactivitylistdatathread.cpp
@@ -0,0 +1,6 @@
+#include "loadactivitylistdatathread.h"
+
+LoadActivityListDataThread::LoadActivityListDataThread()
+{
+
+}
---
+++ loadactivitylistdatathread.h
@@ -0,0 +1,11 @@
+#ifndef LOADACTIVITYLISTDATATHREAD_H
+#define LOADACTIVITYLISTDATATHREAD_H
+
+
+class LoadActivityListDataThread
+{
+public:
+    LoadActivityListDataThread();
+};
+
+#endif // LOADACTIVITYLISTDATATHREAD_H
